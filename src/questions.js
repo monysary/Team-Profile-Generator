f3a055fdc0da101ae2c4b518b607e6a24cf73208
monysary <sary.mony@gmail.com>
2022-12-07 18:23:50 -0800

    Added list questions for inquirer prompt

---
+++ src/questions.js
@@ -0,0 +1,68 @@
+const questions = [
+    {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "managerName"
+    },
+    {
+        type: "input",
+        message: "What is the manager's employee ID?",
+        name: "managerID"
+    },
+    {
+        type: "input",
+        message: "What is the manager's email address?",
+        name: "managerEmail"
+    },
+    {
+        type: "input",
+        message: "What is the manager's office number?",
+        name: "managerOffice"
+    },
+    {
+        type: "list",
+        message: "What team members would you like to add?",
+        name: "teamMember",
+        choices: "Engineer, Intern, None"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "engineerName"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's employee ID?",
+        name: "engineerID"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's email address?",
+        name: "engineerEmail"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's GitHub username?",
+        name: "engineerGitHub"
+    },
+    {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "internName"
+    },
+    {
+        type: "input",
+        message: "What is the intern's employee ID?",
+        name: "internID"
+    },
+    {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "internEmail"
+    },
+    {
+        type: "input",
+        message: "What is the intern's school name?",
+        name: "internSchool"
+    },
+]
