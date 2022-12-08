ea1b22954fe20de335a7edfbe2ee161d7a99bba6
monysary <sary.mony@gmail.com>
2022-12-08 15:05:52 -0800

    Finished writing tests for Employee class

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -18,4 +18,27 @@ describe("Employee", () => {
         const testObj = new employee("John Doe", 123, email);
         expect(testObj.email).toBe(email);
     })
+
+    it("Tests if employee getName method returns employee's name", () => {
+        const name = "John Doe";
+        const testObj = new employee(name);
+        expect(testObj.getName()).toBe(name);
+    })
+
+    it("Tests if employee getID method returns employee's ID", () => {
+        const id = 123;
+        const testObj = new employee("John Doe", id);
+        expect(testObj.getID()).toBe(id);
+    })
+
+    it("Tests if employee getEmail method returns employee's email", () => {
+        const email = "JohnDoe@gmail.com";
+        const testObj = new employee("John Doe", 123, email);
+        expect(testObj.getEmail()).toBe(email);
+    })
+
+    it("Tests if employee getRole method return's 'Employee'", () => {
+        const testObj = new employee("John Doe", 123, "JohnDoe@gmail.com");
+        expect(testObj.getRole()).toBe("Employee");
+    })
 })
